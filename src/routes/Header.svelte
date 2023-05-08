66e78cc06504c436c0b73691b15a84a0063cc4f3
calcalayy <minelandsmc14@gmail.com>
2023-05-08 10:49:38 +0100

    header :pray:

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,4 @@
-<div class="bg-black">
-  <h1 class="text-3xl font-bold text-white text-center py-2">Sirius Events</h1>
+<div class="bg-gray-700 bg-opacity-50 rounded-b-sm flex text-white items-center text-center">
+  <h1 class="text-3xl font-bold text-white text-center p-2">Sirius Events</h1>
+  <p class="px-2 py-1 text-xl">Leaderboard</p>
 </div>
