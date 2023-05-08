548b98a9f3ab71e950e0e1a85171981ddebfb0ac
calcalayy <minelandsmc14@gmail.com>
2023-05-08 10:38:49 +0100

    basic things like icon1!!!11!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Header from "./Header.svelte";
+</script>
+
+<Header />
+
+<svelte:head>
+  <title>Sirius Events</title>
+  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+</svelte:head>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.blue.300);
+  }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,3 @@
+<div class="bg-black">
+  <h1 class="text-3xl font-bold text-white text-center py-2">Sirius Events</h1>
+</div>
