7d22922e2298c74cb8318101c38d246b5ed9d6da
calcalayy <minelandsmc14@gmail.com>
2023-05-08 11:30:58 +0100

    make text look phatter

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,4 @@
-<div class="bg-neutral-900 bg-opacity-25 rounded-b-sm flex text-white items-center">
-  <h1 class="text-3xl font-bold text-white text-center p-2 px-4">Sirius Events</h1>
-  <a class="px-2 text-xl" href="/src/routes/leader.svelte">Leaderboard</a>
-</div>
+<nav class="bg-neutral-900 bg-opacity-25 rounded-b-sm flex text-white items-center">
+  <h1 class="text-2xl font-bold text-white text-center p-2 px-4">Sirius Events</h1>
+  <a class="px-2 text-lg" href="#">Leaderboard</a>
+</nav>
