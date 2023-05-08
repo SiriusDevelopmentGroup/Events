b8aedf2b9853929e1c094f334bd8bfc0f10432fb
calcalayy <minelandsmc14@gmail.com>
2023-05-08 10:58:36 +0100

    made better

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,4 +1,4 @@
 <div class="bg-neutral-900 bg-opacity-25 rounded-b-sm flex text-white items-center">
-  <h1 class="text-3xl font-bold text-white text-center p-2">Sirius Events</h1>
-  <a class="px-2 py-1 text-xl" href="">Leaderboard</a>
+  <h1 class="text-3xl font-bold text-white text-center p-2 px-4">Sirius Events</h1>
+  <a class="px-2 text-xl" href="/src/routes/Leaderboard.svelte">Leaderboard</a>
 </div>
